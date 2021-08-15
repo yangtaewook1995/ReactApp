c18aa62cc833950b70ff943f88e5a84afb6ba308
Wook <wooke1995@gmail.com>
2021-08-15 16:05:01 +0900

    modify App.js

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,17 +1,28 @@
-import './App.css';
-import Hello from './components/Hello';
-import Welcome from './components/Welcome';
-import styles from './App.module.css';
+
+import Header from "./components/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import EmptyPage from "./components/EmptyPage";
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="App">
-            <Hello age = {10}/>
-            <Hello age = {20}/>
-            <Hello age = {30}/>
-
-            {/* <div className = {styles.box}>App</div> */}
+            <Header />
+            <Switch>
+            <Route exact path="/">
+            </Route>
+            <Route path="/day/:day">
+            </Route>
+            <Route path="/create_word">
+            </Route>
+            <Route path="/create_day">
+            </Route>
+            <Route>
+                <EmptyPage />
+            </Route>
+            </Switch>
         </div>
+        </BrowserRouter>
     );
 }
 
